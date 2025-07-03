13080eea72fdc65662a21dd31ffe9d17954f0d3b
jiwon-1226 <jiwon050612@gmail.com>
2025-07-03 15:14:57 +0900

    operator

---
+++ 04_collection/dictionary.py
@@ -0,0 +1,57 @@
+#딕셔너리
+from pdb import line_prefix
+
+profile = {
+    "이름": "김지원",
+    "나이": 21,
+    "취미": ["영화", "야구"],
+    2: 3,
+    4: [1, 2, 3]
+}
+
+#요소 접근
+print(profile["이름"])
+print(profile["취미"][1])
+
+#수정
+profile["나이"] = 22
+print(profile)
+
+#요소 추가
+profile["직업"] = "강사"
+print(profile)
+
+#요소 삭제
+del profile["직업"]
+print(profile)
+# profile.clear() #전체 삭제
+
+#키만 불러오기
+print(profile.keys())
+
+#값만 불러오기
+print(profile.values())
+
+#키와 값 둘 다 불러오기
+print(profile.items())
+
+python_grade = {
+    "kelly": "B",
+    "jason": "A",
+    "ian": "C",
+    "elly": "D"
+}
+
+print(sorted(python_grade.items())) #키 기준 ㅇ르마순 정렬
+print(sorted(python_grade.items(), reverse=True)) #키 기준 내리마순 정렬
+
+student = {}
+#이름을 입력받ㄱ 해당 이름을 킬, 점수를 입력받ㄱ 값을 ㅜ가
+#student = {"김지원": 80}
+
+name = input("이름 : ")
+sore = input(f"{name}의 점수 : ")
+student[name] = sore
+print(student)
+
+
