8d83f0f0c26406dcebbdcfcc62e16fe8c71e3104
jiwon-1226 <jiwon050612@gmail.com>
2025-07-14 12:59:30 +0900

    modules02

---
+++ 12_modules/modules02.py
@@ -0,0 +1,14 @@
+# request
+
+# import requests
+#
+# responses = requests.get("https://www.naver.com/")
+# print(responses.status_code) #웹 서버로부터 밭은 응답의 HTTP 상태코드를 출력
+# print(responses.text)
+
+# pandas는 데이터 조작 및 분석을 위한 라이브러리
+# cvs, Excel, 데이터 베이스 등 다양한 형식의 데이터를 읽고 쓸 수 있으며, 데이터를 정렬, 필터링, 그룹화하는 기능을 제공한다.
+
+import pandas as pd
+
+
